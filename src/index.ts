ef19c59079100939cba360e8e5f0b7e33f151976
Lucas Sousa <lsousatop5771@gmail.com>
2026-03-19 16:29:15 -0300

    feat: add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-console.log("Hello Wolrd!!");
+import "dotenv/config";
 
-const name = "";
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", function (request, reply) {
+  reply.send({ hello: "world" });
+});
+
+// Como estamos usando o ESM pode se usar o try catch sem async/await
+
+try {
+  await fastify.listen({ port: Number(process.env.PORT) || 8081 });
+} catch (error) {
+  fastify.log.error(error);
+  process.exit(1);
+}
