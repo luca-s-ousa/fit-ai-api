93ff15135613f90c5d5308cb95be1bccf23a9917
Lucas Sousa <lsousatop5771@gmail.com>
2026-04-10 14:52:47 -0300

    feat: add get stats endpoint

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ import z from "zod";
 
 import { auth } from "./lib/auth.js";
 import { homeRoutes } from "./routes/home.js";
+import { statsRoutes } from "./routes/stats.js";
 import { workoutPlansRoutes } from "./routes/workout-plan.js";
 
 const app = Fastify({
@@ -64,6 +65,7 @@ await app.register(fastifyApiReference, {
 });
 
 await app.register(homeRoutes, { prefix: "/home" });
+await app.register(statsRoutes, { prefix: "/stats" });
 await app.register(workoutPlansRoutes, { prefix: "/workout-plans" });
 
 app.withTypeProvider<ZodTypeProvider>().route({
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -109,3 +109,23 @@ export const GetWorkoutDayResponseSchema = z.object({
     })
   ),
 });
+
+export const GetStatsQuerySchema = z.object({
+  from: z.iso.date(),
+  to: z.iso.date(),
+});
+
+export const GetStatsResponseSchema = z.object({
+  workoutStreak: z.number(),
+  consistencyByDay: z.record(
+    z.iso.date(),
+    z.object({
+      workoutDayCompleted: z.boolean(),
+      workoutDayStarted: z.boolean(),
+    })
+  ),
+  completedWorkoutsCount: z.number(),
+  conclusionRate: z.number(),
+  totalTimeInSeconds: z.number(),
+});
+
